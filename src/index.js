6dc8d2c8e8c52be9672a55afc4a085d0f5d54113
Alex Lende <ajlende@gmail.com>
2020-01-14 15:24:30 -0600

    Add template for WebGL block

--- src/index.js
+++ src/index.js
@@ -26,9 +26,11 @@ import * as eventBlock from '../blocks/event/src';
 import * as layoutGridBlock from '../blocks/layout-grid/src';
 import * as richImageTools from '../blocks/rich-image/src';
 import * as bauhausCentenaryBlock from '../blocks/bauhaus-centenary/src';
+import * as motionBackgroundBlock from '../blocks/motion-background/src';
 
 // Instantiate the blocks, adding them to our block category
 eventBlock.registerBlock();
 layoutGridBlock.registerBlock();
 richImageTools.registerBlock();
 bauhausCentenaryBlock.registerBlock();
+motionBackgroundBlock.registerBlock();
